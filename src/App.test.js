5adcca71343907078f2b9315b970bb05e58a500d
Chris Spears <zometer@gmail.com>
2022-02-13 23:33:37 -0600

    updating test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search bar', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const searchBar = screen.getByPlaceholderText("Search Alliances");
+  expect(searchBar).toBeInTheDocument();
 });
