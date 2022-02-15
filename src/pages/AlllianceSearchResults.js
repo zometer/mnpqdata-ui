fe4479ddb292fd1402e105bea4b74e151fcb1bea
Chris Spears <zometer@gmail.com>
2022-02-14 19:35:40 -0600

    #2: Adding spinner to alliance search results page

--- src/pages/AlllianceSearchResults.js
+++ src/pages/AlllianceSearchResults.js
@@ -1,9 +1,14 @@
+import { ScaleLoader } from "react-spinners";
+import variables from 'App.scss'
 
 const AllianceSearchResults = ({results, loading}) => { 
-  console.log("results", results);
 
   if (loading) { 
-      return <section className="content"> <p>Loading...</p></section>
+      return ( 
+        <section className="content loading"> 
+          <ScaleLoader color={variables.spinnerColor} loading={loading} />
+        </section>
+      );
   }
 
   if (results.length === 0) { 
