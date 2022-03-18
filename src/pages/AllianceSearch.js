00960abe59a9cef7669c5256b0f306cf09249cbf
Chris Spears <zometer@gmail.com>
2022-03-18 11:22:45 -0500

    Reading search params from redux state and searching via graphql.

--- src/pages/AllianceSearch.js
+++ src/pages/AllianceSearch.js
@@ -1,72 +1,50 @@
-import { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { gql, useQuery } from "@apollo/client";
 import { replaceBreadcrumbs } from "state/slices/uiSlice";
 import AllianceSearchForm from "components/AllianceSearchForm";
 import AllianceSearchResults from "components/AlllianceSearchResults";
 import { ALLIANCE_SEARCH, HOME } from "utils/BreadcrumbEntry";
+import { startAllianceSearch } from "state/slices/searchSlice";
+
+const SEARCH_QUERY = gql`
+query searchAlliances($name: String!) {
+	alliances: searchAlliances(
+		name: $name
+		includeFull: true
+		includePrivate: true
+	) {
+		allianceName
+		allianceType
+		allianceSize
+		allianceMaxSize
+	}
+}
+`;
 
 const AllianceSearch = (props) => { 
-  const [searchParams] = useSearchParams();
-  const [results, setResults] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-  const [shouldSearch, setShouldSearch] = useState( searchParams.getAll("search").length > 0 );
-  const searchApiUrl = window._env.MPQDATA_API_URL + "api/rest/v3/search/alliance/";
-
+  const {name, includeFull, includePrivate, startSearch} = useSelector((state) => state.search.alliance); 
   const dispatch = useDispatch();
+  const {loading, error, data} = useQuery(SEARCH_QUERY, {
+    variables: { name }, 
+    skip: ! startSearch
+  });
+
   useEffect( () => {
     dispatch(replaceBreadcrumbs([HOME, ALLIANCE_SEARCH]));
+    dispatch(startAllianceSearch(false));
   }, [dispatch]);
 
-  const searchAlliances = (searchQuery, includeFull, includePrivate) => { 
-    setLoading(true);
-    includeFull = (includeFull === undefined) ? false : includeFull;
-    includePrivate = (includePrivate === undefined) ? false : includePrivate;
-    let params = new URLSearchParams({"q": searchQuery, "includeFull": includeFull, "includePrivate": includePrivate});
-    let url = new URL(searchApiUrl); 
-    url.search = params.toString(); 
-    fetch(url)
-      .then(checkForError)
-      .then(res => res.json())
-      .then(res => { 
-        setResults(res);
-        setLoading(false);
-      })
-      .catch(e => {
-        console.log("error", e);
-        setLoading(false);
-        setResults([]);
-        setError(e.message);
-      })
-    ;
-  } 
-
-  const checkForError = (res) => { 
-    if (res.ok) {
-      return res;
-    }
-    throw Error("Error querying alliances.")
-  }
-
-  const resolveBoolean = (input) => { 
-      return input === 'true';
-  }
-
-  const includeFull = resolveBoolean( searchParams.get("fullAlliances") );
-  const includePrivate = resolveBoolean( searchParams.get("privateAlliances") );
-
-  if (searchParams.get("search") && shouldSearch) { 
-    setShouldSearch(false); 
-    searchAlliances(searchParams.get("search"), includeFull, includePrivate);    
-  }
+  const errMsg = data?.errors?.[0]?.message || error?.message || undefined;
+  const results = data?.alliances || []; 
 
   return (
     <main>
       <h1>Alliance Search</h1>
-      <AllianceSearchForm query={searchParams.getAll("search")} fullAlliances={includeFull} privateAlliances={includePrivate}/>
-      <AllianceSearchResults results={results} loading={loading} error={error}/>
+      <AllianceSearchForm />
+      <AllianceSearchResults results={results} loading={loading} error={errMsg}/>
     </main>
   );
 }
+
 export default AllianceSearch;
