a26fcc697b626c8d871bf17bc2b83ccbcb851f2c
Chris Spears <zometer@gmail.com>
2022-03-18 11:24:47 -0500

    Making links plural for consistency

--- src/utils/BreadcrumbEntry.js
+++ src/utils/BreadcrumbEntry.js
@@ -4,10 +4,10 @@ const BreadcrumbEntry = (name, href) => ({name, href})
 export default BreadcrumbEntry ; 
 export const HOME = BreadcrumbEntry("Home", "/"); 
 export const DASHBOARD = BreadcrumbEntry("Dashboard", "/dashboard"); 
-export const ALLIANCE = BreadcrumbEntry("Alliance", "/alliance"); 
-export const ALLIANCE_SEARCH = BreadcrumbEntry("Alliance Search", "/search/alliance"); 
+export const ALLIANCE = BreadcrumbEntry("Alliance", "/alliances"); 
+export const ALLIANCE_SEARCH = BreadcrumbEntry("Alliance Search", "/search/alliances"); 
 export const ROSTER = BreadcrumbEntry("Roster", "/roster"); 
 
-export const allianceBreadcrumb = (allianceName) => (BreadcrumbEntry(allianceName, `/alliance/${allianceName}`));
-export const rosterBreadcrumb = (rosterName) => BreadcrumbEntry(rosterName, `/roster/${rosterName}`);
-export const rosterCharBreadcrumb = (rosterName, instanceId) => BreadcrumbEntry(rosterName, `/roster/${rosterName}/${instanceId}`);
+export const allianceBreadcrumb = (allianceName) => (BreadcrumbEntry(allianceName, `/alliances/${allianceName}`));
+export const rosterBreadcrumb = (rosterName) => BreadcrumbEntry(rosterName, `/rosters/${rosterName}`);
+export const rosterCharBreadcrumb = (rosterName, instanceId) => BreadcrumbEntry(rosterName, `/rosters/${rosterName}/${instanceId}`);
